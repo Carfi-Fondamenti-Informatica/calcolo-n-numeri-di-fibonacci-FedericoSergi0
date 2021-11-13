fd5f9ec03bd0b98c6ba3a35035c5b5a09115f770
FedericoSergi0 <91880033+FedericoSergi0@users.noreply.github.com>
2021-11-13 12:47:01 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 using namespace std;
-int n;
+int main() {
+   int n;
    int a=1, b=1, c;
    cin >> n;
    if(n<2){
@@ -15,5 +16,5 @@ int n;
       cout << c <<endl;
   }
    }
-   return 0;
+    return 0;
 }
