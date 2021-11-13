a79ba19791a55cb81326d31129a20dcea418b3cd
FedericoSergi0 <91880033+FedericoSergi0@users.noreply.github.com>
2021-11-13 12:41:43 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,16 @@
 using namespace std;
 
 int main() {
+   int n;
+   int a=1, b=1, c;
+   cin >> n;
+  cout << a <<endl;
+  cout << b <<endl;
+  for(int i=0;i<=(n-2);i++){
+      c=a+b;
+      a=b;
+      b=c;
+      cout << c <<endl;
+   }
    return 0;
 }
